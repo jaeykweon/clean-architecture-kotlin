7eef0e0afde21a0d005cdeb44b21f9a246ea23b3
jay <jaeykweon@naver.com>
2023-07-27 15:30:50 +0900

    init: project initialize

---
+++ src/main/kotlin/org/idd/cleanarchitecturekotlin/CleanArchitectureKotlinApplication.kt
@@ -0,0 +1,11 @@
+package org.idd.cleanarchitecturekotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CleanArchitectureKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<CleanArchitectureKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/org/idd/cleanarchitecturekotlin/CleanArchitectureKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.idd.cleanarchitecturekotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CleanArchitectureKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
