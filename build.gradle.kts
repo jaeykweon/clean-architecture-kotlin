0e9f37cc5705c4f02c8d13920f1bffb8314c8065
jay <jaeykweon@naver.com>
2023-08-03 02:40:21 +0900

    config: dependency 추가 - mockito-kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.0.0")
 }
 
 tasks {
